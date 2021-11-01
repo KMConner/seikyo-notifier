62d47ed561178ac88ed8cd201488f760dee4ad47
KMConner <KMConner@users.noreply.github.com>
2021-11-01 22:23:23 +0900

    make a new request to sign in

--- src/bin/token_getter.rs
+++ src/bin/token_getter.rs
@@ -1,6 +1,13 @@
 use seikyo_notifier::seikyo_client::auth;
+use std::env;
 
 fn main() {
-    auth::get_token();
-    println!("Hello World!!")
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: {} [user id] [password]", args[0]);
+        return;
+    }
+    let user_id: String = args[1].clone();
+    let password: String = args[2].clone();
+    auth::get_token(user_id, password).expect("pien");
 }
--- src/seikyo_client/auth.rs
+++ src/seikyo_client/auth.rs
@@ -1,3 +1,35 @@
-pub fn get_token(){
-    println!("hello world");
+use std::collections::HashMap;
+
+use chrono;
+use chrono::TimeZone;
+use reqwest;
+
+const SIGN_IN_ENDPOINT: &str = "https://mb.seikyou.jp/mobileapp_common/tohoku/getToken2.do";
+
+pub fn get_token(id: String, pass: String) -> Result<String, reqwest::Error> {
+    let jst_datetime = chrono::FixedOffset::east(9 * 3600).from_utc_datetime(&chrono::Utc::now().naive_utc());
+    let datetime_param = jst_datetime.format("%Y-%m-%d %H:%M:%S").to_string();
+    let mut password_hash_original = id.to_owned();
+    password_hash_original.push_str(&pass);
+    let digest = md5::compute(password_hash_original);
+    let digest_vec = digest.to_vec();
+    let digest_hex = hex::encode(digest_vec);
+
+    let mut params = HashMap::new();
+    params.insert("appVersion", "5.0.0");
+    params.insert("mpLoginId", id.as_str());
+    params.insert("mpPassword", digest_hex.as_str());
+    params.insert("nativeAppId", "6651000001");
+    params.insert("userAgent", "iOS 15.0.2");
+    params.insert("userDatetime", datetime_param.as_str());
+
+    let client = reqwest::blocking::Client::new();
+    let response = client.post(SIGN_IN_ENDPOINT)
+        .form(&params)
+        .send()?
+        .text()?;
+
+    println!("{:?}", response);
+
+    return Result::Ok(String::from(""));
 }
